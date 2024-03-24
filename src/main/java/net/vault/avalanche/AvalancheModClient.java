be4694edf0bcae0756c2736775f485dadcd8de2c
TheRealVault76 <austingooch2003@gmail.com>
2024-03-24 10:29:52 -0500

    Initial commit

---
+++ src/main/java/net/vault/avalanche/AvalancheModClient.java
@@ -0,0 +1,10 @@
+package net.vault.avalanche;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AvalancheModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
