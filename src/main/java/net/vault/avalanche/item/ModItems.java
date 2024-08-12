ab646569e8eb60027283f792ed0a574e171e349b
TheRealVault76 <102394812+TheRealVault76@users.noreply.github.com>
2024-08-12 15:01:15 -0500

    Update ModItems.java

--- src/main/java/net/vault/avalanche/item/ModItems.java
+++ src/main/java/net/vault/avalanche/item/ModItems.java
@@ -1,37 +1,19 @@
 package net.vault.avalanche.item;
 
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
-import net.minecraft.item.ArmorItem;
 import net.minecraft.item.Item;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 import net.vault.avalanche.Avalanche;
-import net.vault.avalanche.item.custom.IrregulariumArmorItem;
 
 public class ModItems {
 
-    public static final Item ROUGH_PRODIUM = registerItem("rough_prodium",
-            new Item(new FabricItemSettings()));
-    public static final Item DIAMOND_INGOT = registerItem("diamond_ingot",
-            new Item(new FabricItemSettings()));
-    public static final Item DRAKONIUM = registerItem("drakonium",
-            new Item(new FabricItemSettings()));
-    public static final Item RAW_IRREGULARIUM = registerItem("raw_irregularium",
-            new Item(new FabricItemSettings()));
-    public static final Item IRREGULARIUM_BIT = registerItem("irregularium_bit",
-            new Item(new FabricItemSettings()));
-    public static final Item IRREGULARIUM_CORE = registerItem("irregularium_core",
+    public static final Item DRAGON_SCALE = registerItem("dragon_scale",
             new Item(new FabricItemSettings()));
 
-    public static final Item IRREGULARIUM_HELMET = registerItem("irregularium_helmet",
-            new IrregulariumArmorItem(ModArmorMaterials.IRREGULARIUM, ArmorItem.Type.HELMET, new FabricItemSettings()));
-    public static final Item IRREGULARIUM_CHESTPLATE = registerItem("irregularium_chestplate",
-            new IrregulariumArmorItem(ModArmorMaterials.IRREGULARIUM, ArmorItem.Type.CHESTPLATE, new FabricItemSettings()));
-    public static final Item IRREGULARIUM_LEGGINGS = registerItem("irregularium_leggings",
-            new IrregulariumArmorItem(ModArmorMaterials.IRREGULARIUM, ArmorItem.Type.LEGGINGS, new FabricItemSettings()));
-    public static final Item IRREGULARIUM_BOOTS = registerItem("irregularium_boots",
-            new IrregulariumArmorItem(ModArmorMaterials.IRREGULARIUM, ArmorItem.Type.BOOTS, new FabricItemSettings()));
+
+
 
 
     private static Item registerItem (String name, Item item) {
