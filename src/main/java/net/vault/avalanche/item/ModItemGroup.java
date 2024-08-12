fbeba6933fb3dc708f3adf42484107b3e4b64d6d
TheRealVault76 <102394812+TheRealVault76@users.noreply.github.com>
2024-08-12 15:00:53 -0500

    Update ModItemGroup.java

--- src/main/java/net/vault/avalanche/item/ModItemGroup.java
+++ src/main/java/net/vault/avalanche/item/ModItemGroup.java
@@ -11,23 +11,13 @@ import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 
 public class ModItemGroup {
-    public static final ItemGroup DIAMOND_INGOT_GROUP = Registry.register(Registries.ITEM_GROUP,
-            new Identifier(Avalanche.MOD_ID, "diamond_ingot_group"),
+    public static final ItemGroup DRAGON_SCALE_GROUP = Registry.register(Registries.ITEM_GROUP,
+            new Identifier(Avalanche.MOD_ID, "dragon_scale_group"),
             FabricItemGroup.builder().displayName(Text.translatable("itemgroup.diamond_ingot_group"))
-                    .icon(() -> new ItemStack(ModItems.DIAMOND_INGOT)).entries((displayContext, entries) -> {
+                    .icon(() -> new ItemStack(ModItems.DRAGON_SCALE)).entries((displayContext, entries) -> {
 
-                        entries.add(ModItems.ROUGH_PRODIUM);
-                        entries.add(ModBlocks.ROUGH_PRODIUM_BLOCK);
-                        entries.add(ModItems.DIAMOND_INGOT);
-                        entries.add(ModItems.DRAKONIUM);
-                        entries.add(ModItems.RAW_IRREGULARIUM);
-                        entries.add(ModItems.IRREGULARIUM_BIT);
-                        entries.add(ModItems.IRREGULARIUM_CORE);
-
-                        entries.add(ModItems.IRREGULARIUM_HELMET);
-                        entries.add(ModItems.IRREGULARIUM_CHESTPLATE);
-                        entries.add(ModItems.IRREGULARIUM_LEGGINGS);
-                        entries.add(ModItems.IRREGULARIUM_BOOTS);
+                        entries.add(ModItems.DRAGON_SCALE);
+                        entries.add(ModBlocks.DRAGON_SCALE_BLOCK);
 
 
                     }).build());
@@ -36,3 +26,4 @@ public class ModItemGroup {
 
     }
 }
+
