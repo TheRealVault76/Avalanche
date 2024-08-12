1681ca09ef1c597acb6f93cc9e022bca4afd23c4
TheRealVault76 <102394812+TheRealVault76@users.noreply.github.com>
2024-08-12 14:56:27 -0500

    Add files via upload

---
+++ src/main/java/net/vault/avalanche/block/custom/SuperCombinationStationBlock.java
@@ -0,0 +1,15 @@
+package net.vault.avalanche.block.custom;
+
+public class SuperCombinationStationBlock {
+}
+
+/*
+This is 1 block, with 4 parts. It should be possible to define the entire block in just one class.
+The block models are already in the files.
+The block takes up a 2*1*2 area, and can rotate based on the direction the player is facing.
+The block looks like this:
+    SCS 3 | SCS 4
+    SCS 2 | SCS 1
+Being only one block tall, this can be presented in 2D.
+You do not need to define the block entity aspect yet, just the actual block itself.
+*/
